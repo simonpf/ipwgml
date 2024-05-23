116ae4760a5939d4cf3032f58c706b7ae0e9f2cd
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2024-05-23 13:56:55 -0600

    Fix requirements and add test for spatial variant of SPR dataset.

--- ipwgml/pytorch/data.py
+++ ipwgml/pytorch/data.py
@@ -164,22 +164,128 @@ class SPRTabular(Dataset):
         return inpt, target
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class SPRSpatial:
-    pass
+    """
+    Dataset class providing access to the spatial variant of the satellite precipitation retrieval
+    benchmark dataset.
+    """
+    def __init__(
+        self,
+        sensor: str,
+        geometry: str,
+        split: str,
+        retrieval_input: List[str] = None,
+        ancillary_variables: Optional[List[str]] = None,
+        ipwgml_path: Optional[Path] = None,
+        download: bool = True,
+    ):
+        """
+        Args:
+            sensor: The sensor for which to load the benchmark dataset.
+            geometry: Whether to load native or regridded observations.
+            split: Whether to load training ('train'), validation ('val'), or
+                 test ('test') splits.
+            retrieval_input: List of the retrieval inputs to load. Available inputs include
+                 - 'pmw': Passive microwave observations
+                 - 'geo': GOES16 geostationary observations
+                 - 'geo_ir': Single-channels geostationary IR observations.
+                 - 'ancillary': Ancillary data.
+                Defaults to loading all inputs.
+            ancillary_variables: List of the ancillary variables to include in the loaded ancillary data.
+            ipwgml_path: Path containing or to which to download the IPWGML data.
+            download: If 'True', missing data will be downloaded upon dataset creation. Otherwise, only
+                locally available files will be used.
+        """
+        super().__init__()
+
+        if ipwgml_path is None:
+            ipwgml_path = config.get_data_path()
+        else:
+            ipwgml_path = Path(ipwgml_path)
+
+        if not sensor.lower() in ["gmi", "mhs"]:
+            raise ValueError(
+                "Sensor must be one of ['gmi', 'mhs']."
+            )
+        self.sensor = sensor.lower()
+
+        if not geometry.lower() in ["gridded", "native"]:
+            raise ValueError(
+                "Geomtry must be one of ['gridded', 'native']."
+            )
+        self.geometry = geometry.lower()
+
+        if not split.lower() in ["train", "val", "test"]:
+            raise ValueError(
+                "Split must be one of ['train', 'val', 'test']"
+            )
+        self.split = split
+        self.retrieval_input = retrieval_input
+
+        if ancillary_variables is None:
+            ancillary_variables = ANCILLARY_VARIABLES
+        self.ancillary_variables = ancillary_variables
+
+        self.pmw = None
+        self.geo = None
+        self.geo_ir = None
+        self.ancillary = None
+        self.target = None
+
+        dataset = f"spr/{self.sensor}/{self.geometry}/spatial/{self.split}/"
+        for inpt in retrieval_input:
+            if inpt.lower() not in ["pmw", "geo", "geo_ir", "ancillary"]:
+                raise ValueError(
+                    "Encountered invalid input '%s'."
+                )
+            if download:
+                download_missing(dataset + inpt, ipwgml_path)
+            files = list((ipwgml_path / dataset / inpt).glob("*.nc"))
+            setattr(self, inpt, np.array(files))
+
+
+        if download:
+            download_missing(dataset + "target", ipwgml_path)
+        files = list((ipwgml_path / dataset / "target").glob("*.nc"))
+        self.target = np.array(files)
+
+
+    def __len__(self) -> int:
+        """
+        The number of samples in the dataset.
+        """
+        return len(self.target)
+
+    def __getitem__(self, ind: int) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
+        """
+        Load sample from dataset.
+        """
+        inpt = {}
+        if self.pmw is not None:
+            with xr.open_dataset(self.pmw[ind]) as data:
+                obs = data.observations.data.transpose((2, 0, 1))
+                inpt["obs_pmw"] = torch.tensor(obs.astype(np.float32))
+                eia = data.earth_incidence_angle.data
+                inpt["eia_pmw"] = torch.tensor(obs.astype(np.float32))
+
+        if self.geo is not None:
+            with xr.open_dataset(self.geo[ind]) as data:
+                obs = data.observations.data.transpose((2, 0, 1))
+                inpt["obs_geo"] = torch.tensor(obs.astype(np.float32))
+
+        if self.geo_ir is not None:
+            with xr.open_dataset(self.geo_ir[ind]) as data:
+                obs = data.observations.data.transpose((2, 0, 1))
+                inpt["obs_geo_ir"] = torch.tensor(obs.astype(np.float32))
+
+        if self.ancillary is not None:
+            with xr.open_dataset(self.ancillary[ind]) as data:
+                for var in self.ancillary_variables:
+                    anc = np.stack(data[var].data.astype(np.float32))
+                    inpt["ancillary"] = torch.tensor(anc)
+
+        with xr.open_dataset(self.target[ind]) as data:
+            target = data.surface_precip.data.astype(np.float32)
+            target = torch.tensor(target)
+
+        return inpt, target
--- tests/pytorch/test_data.py
+++ tests/pytorch/test_data.py
@@ -1,7 +1,7 @@
 """
 Tests for the ipwgml.pytorch.data module.
 """
-from ipwgml.pytorch.data import SPRTabular
+from ipwgml.pytorch.data import SPRTabular, SPRSpatial
 
 
 def test_dataset_spr_tabular(spr_gmi_native_tabular_train):
@@ -24,3 +24,25 @@ def test_dataset_spr_tabular(spr_gmi_native_tabular_train):
     assert x["obs_pmw"].shape == (13,)
     assert "ancillary" in x
     assert y.numel() == 1
+
+
+def test_dataset_spr_spatial(spr_gmi_gridded_spatial_train):
+    """
+    Test loading of tabular data from the SPR dataset.
+    """
+    data_path = spr_gmi_gridded_spatial_train
+    dataset = SPRSpatial(
+        sensor="gmi",
+        geometry="gridded",
+        split="train",
+        retrieval_input=["pmw", "ancillary"],
+        ipwgml_path=data_path,
+        download=False
+    )
+
+    assert len(dataset) > 0
+    x, y = next(iter(dataset))
+    assert "obs_pmw" in x
+    assert x["obs_pmw"].shape == (13, 256, 256)
+    assert "ancillary" in x
+    assert y.shape == (256, 256)
