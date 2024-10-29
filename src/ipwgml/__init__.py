3f44625d8727f44c3658191f975ea950ae4569de
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2024-10-28 22:27:20 -0600

    Add hd5plugin import to toplevel module file.

---
+++ src/ipwgml/__init__.py
@@ -0,0 +1,9 @@
+"""
+ipwgml
+======
+
+The ipwgml Python package provides access and evaluation functionality for
+machine-learning datasets developed by the machine-learning working group
+of the International Precipitation Working Group (IPWG).
+"""
+import hdf5plugin
